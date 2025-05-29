0f8251be28c896de1588218e894d37379769189d
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-29 14:48:02 +0200

    added cursor-pointer to Button

---
+++ app/components/Nav.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { signOut, useSession } from '@/app/auth-client';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+export default function Nav() {
+  const { data } = useSession();
+
+  return (
+    <nav className='flex gap-2'>
+      {data ? (
+        <>
+          <Link href='/create-post'>Create</Link>
+          <Button
+            className='cursor-pointer'
+            onClick={() => signOut()}>
+            Signout
+          </Button>
+        </>
+      ) : (
+        <>
+        <Link href='/signin'>Signin</Link>
+        </>
+      )}
+    </nav>
+  );
+}
