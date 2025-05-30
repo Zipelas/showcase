f6e9800cd92fa257904a55769cb2786f8aaad669
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-30 19:35:41 +0200

    styling button

--- app/signin/providers/github-button.tsx
+++ app/signin/providers/github-button.tsx
@@ -7,13 +7,13 @@ import Image from 'next/image';
 export default function GithubButton() {
   return (
     <Button
-      className='bg-[#8D6E63] text-[#FAF7F7] p-3 rounded text-lg flex items-center gap-2'
+      className='bg-[#8D6E63] font-bold text-[#FAF7F7] p-12 rounded text-lg flex items-center gap-2 cursor-pointer'
       onClick={() => signIn.social({ provider: 'github' })}>
       <Image
-        src='/images/github.png'
+        src='/icons/github.png'
         alt='Github-ikon'
-        width={16}
-        height={16}
+        width={32}
+        height={32}
       />
       Signin with Github
     </Button>
